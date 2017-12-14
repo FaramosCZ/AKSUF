32910e1807002a567e31202ef7e36991b544797c
Michal Schorm <mschorm@redhat.com>
2017-12-14 03:46:48 +0100

    Added coordinates sorting Added generator of all near moves Added coordinates checks Added history stack number operations

--- main.c
+++ main.c
@@ -62,8 +62,8 @@ typedef struct history
  char board[HEIGHT][WIDTH];                      // Each history entry will hold the whole playing board status
  struct history * history_older;                 // Pointer to "older" history - a history closer to the game END
 
- int history_stack, position, combination, type; // Serial number of the history entry (mostly for debugging); and some info about the triplets		// yet UNUSED
- int orientation;                                // Some info about the triplet    // USED
+ int position, combination, type;                // Serial number of the history entry (mostly for debugging); and some info about the triplets		// yet UNUSED
+ int orientation, history_stack;                 // Some info about the triplet    // USED
 
  int x1, y1, x2, y2, x3, y3;                     // As well as coordinates of all 3 gems of that triplet
 } history;
@@ -75,7 +75,56 @@ history * HISTORY_LATEST = NULL;                 // Pointer to the history one-w
 // -----------------------------------------------------------------------------
 // FUNCTION HEADERS
 
-int check_push_space(history * history_entry, int maximal_x, int minimal_y);
+int check_push_space(history * history_entry);
+
+
+
+// -----------------------------------------------------------------------------
+// SUPPORT FUNCTIONS
+
+void order_coordinates_and_get_orientation(history * history_entry)
+{
+ int memory;
+
+ if( history_entry->y1 == history_entry->y2 ){ history_entry->orientation=VERTICAL;} else{history_entry->orientation=HORIZONTAL;}
+
+ // Order always for bottom left corner;
+ //       X1>X2>X3; Y1<Y2<Y3
+ for(int i=2; i!=0; i--)
+   {
+    if( history_entry->orientation == VERTICAL )
+      {
+       if( history_entry->x1 < history_entry->x2)
+         {
+          memory = history_entry->x1;
+          history_entry->x1 = history_entry->x2;
+          history_entry->x2 = memory;
+         }
+       if( history_entry->x2 < history_entry->x3)
+         {
+          memory = history_entry->x2;
+          history_entry->x2 = history_entry->x3;
+          history_entry->x3 = memory;
+         }
+      }
+    else
+      {
+       if( history_entry->y1 > history_entry->y2)
+         {
+          memory = history_entry->y1;
+          history_entry->y1 = history_entry->y2;
+          history_entry->y2 = memory;
+         }
+       if( history_entry->y2 > history_entry->y3)
+         {
+          memory = history_entry->y2;
+          history_entry->y2 = history_entry->y3;
+          history_entry->y3 = memory;
+         }
+      }
+   }
+
+}
 
 
 
@@ -91,12 +140,10 @@ void history_initialize()
  for( int x=0 ; x<HEIGHT ; x++) for( int y=0 ; y<WIDTH ; y++) HISTORY_LATEST->board[x][y]=PLACEHOLDER;
  // Prepare other variables
  HISTORY_LATEST->history_older = NULL;
- HISTORY_LATEST->history_stack = 0;
- HISTORY_LATEST->orientation = HISTORY_LATEST->position = HISTORY_LATEST->combination = HISTORY_LATEST->type = 0;
+ HISTORY_LATEST->history_stack = HISTORY_LATEST->orientation = HISTORY_LATEST->position = HISTORY_LATEST->combination = HISTORY_LATEST->type = 0;
  HISTORY_LATEST->x1 = HISTORY_LATEST->x2 = HISTORY_LATEST->x3 = HISTORY_LATEST->y1 = HISTORY_LATEST->y2 = HISTORY_LATEST->y3 = 0;
 
  // HERE is the place to set initial field pattern:
-
  HISTORY_LATEST->board[HEIGHT-4][WIDTH/2-1] = 'C';
  HISTORY_LATEST->board[HEIGHT-3][WIDTH/2] = 'C';
  HISTORY_LATEST->board[HEIGHT-2][WIDTH/2] = 'C';
@@ -125,20 +172,8 @@ int history_push(int x1, int x2, int x3, int y1, int y2, int y3, int color)
  // Copy the board from the last history entry
  memcpy(history_entry->board, HISTORY_LATEST->board, SIZE);
  // Prepare other variables
- // history_entry->history_stack = HISTORY_LATEST->history_stack++;
+ history_entry->history_stack = HISTORY_LATEST->history_stack+1;
  history_entry->history_older = HISTORY_LATEST;
-
- // Find the edge values
- int maximal_x = x1, minimal_y = y1;
- if( x1>x2 && x1>x3 ){maximal_x = x1; history_entry->orientation=VERTICAL;}
- if( x2>x1 && x2>x3 ){maximal_x = x2; history_entry->orientation=VERTICAL;}
- if( x3>x2 && x3>x1 ){maximal_x = x3; history_entry->orientation=VERTICAL;}
- if( y1<y2 && y1<y3 ){minimal_y = y1; history_entry->orientation=HORIZONTAL;}
- if( y2<y1 && y2<y3 ){minimal_y = y2; history_entry->orientation=HORIZONTAL;}
- if( y3<y2 && y3<y1 ){minimal_y = y3; history_entry->orientation=HORIZONTAL;}
-
- // Check rules of game, if the move can be really placed
- if( check_push_space(history_entry, maximal_x, minimal_y) != 0 ){ free(history_entry); return -1; }
 
  history_entry->x1 = x1;
  history_entry->x2 = x2;
@@ -147,13 +182,18 @@ int history_push(int x1, int x2, int x3, int y1, int y2, int y3, int color)
  history_entry->y2 = y2;
  history_entry->y3 = y3;
 
+ // Find the edge values and orientation
+ order_coordinates_and_get_orientation(history_entry);
+
+ // Check rules of game, if the move can be really placed
+ if( check_push_space(history_entry) != 0 ){ free(history_entry); return -1; }
+
  // Make space for the triplet
- if( history_entry->orientation == VERTICAL ){ for( int x=3 ; x<=maximal_x ; x++ ) history_entry->board[x-3][y1]=history_entry->board[x][y1]; }
- else{ for( int y=minimal_y ; y<=minimal_y+2 ; y++ ){for( int x=1 ; x<=maximal_x ; x++ ) history_entry->board[x-1][y]=history_entry->board[x][y];} }
+ if( history_entry->orientation == VERTICAL ){ for( int x=3 ; x<=history_entry->x1 ; x++ ) history_entry->board[x-3][y1]=history_entry->board[x][y1]; }
+ else{ for( int y=history_entry->y1 ; y<=history_entry->y1+2 ; y++ ){for( int x=1 ; x<=history_entry->x1 ; x++ ) history_entry->board[x-1][y]=history_entry->board[x][y];} }
 
  // Place the triplet oo the playing board
  history_entry->board[x1][y1] = history_entry->board[x2][y2] = history_entry->board[x3][y3] = color+65; // The "+65" will move the gem number in ASCII
-
 
  HISTORY_LATEST = history_entry;
 
@@ -294,17 +334,17 @@ int check_triplets(){ return check_triplets_specific( HISTORY_LATEST ); }
 
 
 
-int check_push_space(history * history_entry, int maximal_x, int minimal_y)
+int check_push_space(history * history_entry)
 {
  int free_space, x, y;
 
  if( history_entry->orientation==VERTICAL ) // vertical triplet
    {
     free_space=0;
-    for( x=0 ; x<=maximal_x ; x++ ){if( history_entry->board[x][minimal_y] == PLACEHOLDER ) free_space++; }
+    for( x=0 ; x<=history_entry->x1 ; x++ ){if( history_entry->board[x][history_entry->y1] == PLACEHOLDER ) free_space++; }
     // If there is not enough space verticaly for the triplet, fail.
     if( free_space < 3 ) return -1;
-    for( free_space=0 ; x<HEIGHT ; x++ ){if( history_entry->board[x][minimal_y] == PLACEHOLDER ) free_space++; }
+    for( free_space=0 ; x<HEIGHT ; x++ ){if( history_entry->board[x][history_entry->y1] == PLACEHOLDER ) free_space++; }
     // If there is any free space in the column anywhere below the triplet, fail.
     if( free_space != 0 ) return -1;
    }
@@ -313,10 +353,10 @@ int check_push_space(history * history_entry, int maximal_x, int minimal_y)
     for( y=0 ; y<3 ; y++ ) // 3 columns
       {
        free_space=0;
-       for( x=0 ; x<=maximal_x ; x++ ){if( history_entry->board[x][minimal_y+y] == PLACEHOLDER ) free_space++; }
+       for( x=0 ; x<=history_entry->x1 ; x++ ){if( history_entry->board[x][history_entry->y1+y] == PLACEHOLDER ) free_space++; }
        // If there is not enough space horizontaly for the triplet, fail.
        if( free_space < 1 ) return -1;
-       for( free_space=0 ; x<HEIGHT ; x++ ){if( history_entry->board[x][minimal_y+y] == PLACEHOLDER ) free_space++; }
+       for( free_space=0 ; x<HEIGHT ; x++ ){if( history_entry->board[x][history_entry->y1+y] == PLACEHOLDER ) free_space++; }
        // If there is any free space in the column anywhere below the triplet, fail.
        if( free_space != 0 ) return -1;
       }
@@ -325,17 +365,86 @@ int check_push_space(history * history_entry, int maximal_x, int minimal_y)
  return 0;
 }
 
+
+
 // -----------------------------------------------------------------------------
 // GENERATE MOVE
 
+int generate_move_shuffle()
+{
+ //-------------------------------------------
+ // TODO:
+ //   1) Iterate through all free positions around the triplet
+ //   2) place next triplet, run checks
+ printf("generate_move():");
+ print_board();
+ printf("\nNuber of active triplets: %d\n", check_number_of_triplets_active() );
+ printf("\nNuber of triplets one move away: %d\n", check_number_of_triplets_one_move_away() );
+ printf("\nTotal ^: %d\n", check_triplets() );
+ // --------------------------
+
+ history_delete_last_entry();
+
+ return 0;
+}
+
+
+
+int generate_move_check(int x1, int x2, int x3, int y1, int y2, int y3, int color, int history_stack_before)
+{
+ if( x1 < 0 || x2 < 0 || x3 < 0 || y1 < 0 || y2 < 0 || y3 < 0 ) return -1;
+ if( x1 > HEIGHT-1 || x2 > HEIGHT-1 || x3 > HEIGHT-1 ) return -1;
+ if( y1 > WIDTH-1 || y2 > WIDTH-1 || y3 > WIDTH-1 ) return -1;
+
+ history_push(x1, x2, x3, y1, y2, y3, color);
+ if( HISTORY_LATEST->history_stack == history_stack_before ) return -2;
+
+ if( check_number_of_triplets_active() != 2 ) return -3;
+
+ generate_move_shuffle();
+
+ return 0;
+}
+
+
+
 int generate_move()
 {
+ int x = HISTORY_LATEST->x1;
+ int y = HISTORY_LATEST->y1;
+ int history_stack_before = HISTORY_LATEST->history_stack;
 
-   printf("generate_move():");
-   print_board();
-   printf("\nNuber of active triplets: %d\n", check_number_of_triplets_active() );
-   printf("\nNuber of triplets one move away: %d\n", check_number_of_triplets_one_move_away() );
-   printf("\nTotal ^: %d\n", check_triplets() );
+ // Iterate for every color
+ for(int color=0 ; color<COLOR_COUNT ; color++)
+   {
+    if(HISTORY_LATEST->orientation == VERTICAL)
+      {
+       // Last triplet was VERTICAL and new triplet will be VERTICAL too
+       for(int i=0 ; i<5 ; i++) generate_move_check(x-4 + i,x-3 + i,x-2 + i,y-1,y-1,y-1,color, history_stack_before);
+       for(int i=0 ; i<4 ; i++) generate_move_check(x-5 + i,x-4 + i,x-3 + i,y,y,y,color, history_stack_before);
+       for(int i=0 ; i<5 ; i++) generate_move_check(x-4 + i,x-3 + i,x-2 + i,y+1,y+1,y+1,color, history_stack_before);
+       // Last triplet was VERTICAL and new triplet will be HORIZONTAL
+       for(int i=0 ; i<3 ; i++) generate_move_check(x-3,x-3,x-3,y-2 + i,y-1 + i,y + i,color, history_stack_before);
+       for(int i=0 ; i<5 ; i++) generate_move_check(x-2,x-2,x-2,y-3 + i,y-2 + i,y-1 + i,color, history_stack_before);
+       for(int i=0 ; i<5 ; i++) generate_move_check(x-1,x-1,x-1,y-3 + i,y-2 + i,y-1 + i,color, history_stack_before);
+       for(int i=0 ; i<5 ; i++) generate_move_check(x,x,x,y-3 + i,y-2 + i,y-1 + i,color, history_stack_before);
+       for(int i=0 ; i<3 ; i++) generate_move_check(x+1,x+1,x+1,y-2 + i,y-1 + i,y + i,color, history_stack_before);
+      }
+    else
+      {
+       // Last triplet was HORIZONTAL and new triplet will be VERTICAL
+       for(int i=0 ; i<3 ; i++) generate_move_check(x-2 + i,x-1 + i,x + i,y-1,y-1,y-1,color, history_stack_before);
+       for(int i=0 ; i<3 ; i++)
+         {
+          generate_move_check(x-3,x-2,x-1,y+i,y+i,y+i,color, history_stack_before);
+          generate_move_check(x-2,x-1,x,y+i,y+i,y+i,color, history_stack_before);
+         }
+       for(int i=0 ; i<3 ; i++) generate_move_check(x-2 + i,x-1 + i,x + i,y+3,y+3,y+3,color, history_stack_before);
+       // Last triplet was HORIZONTAL and new triplet will be HORIZONTAL too
+       for(int i=0 ; i<5 ; i++) generate_move_check(x-1,x-1,x-1,y-2 + i,y-1 + i,y + i,color, history_stack_before);
+       for(int i=0 ; i<7 ; i++) generate_move_check(x,x,x,y-3 + i,y-2 + i,y-1 + i,color, history_stack_before);
+      }
+   }
 
  return 0;
 }
